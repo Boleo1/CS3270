9f7c8dc43ca3b3871ded88177ebcec3d6946da12
Boleo <beaulieu.tony@yahoo.com>
2024-01-11 13:50:20 -0600

    Starting PHP file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Anthony"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Index</title>
+</head>
+<body>
+<?php 
+"Hello my name is echo($name)." 
+?>
+</body>
+</html>
