92a83b0cb296e4f963636fb1f1b06b9169f92e93
Boleo <beaulieu.tony@yahoo.com>
2024-02-05 23:39:16 -0600

    Starting banking website

---
+++ banking/bank.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bank | Home</title>
+</head>
+<body>
+    This is the home page.
+
+    <a href="index.php">Click to go back</a>
+</body>
+</html>
---
+++ banking/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bank Login</title>
+</head>
+<body>
+    <form  class="loginForm" action="bank.php" method="GET">
+        <label for="userName" >Username: <label><input type="text" id="userName" name="userName"><br>
+        <label for="userPassword" >Password: <label><input type="text" id="userPassword" name="userPassword"><br>
+        <input class="loginSubmit"type="submit" placeholder="Submit"> 
+    </form>
+    <a href="/CS3270/index.php"> Click to go back</a>
+</body>
+</html>
