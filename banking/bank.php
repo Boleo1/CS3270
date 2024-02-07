8e150c0161dcfe490b6162bbc7f88f73bc74c01a
Boleo <beaulieu.tony@yahoo.com>
2024-02-06 20:26:27 -0600

    uploading to work on laptop downstairs :D

--- banking/bank.php
+++ banking/bank.php
@@ -1,13 +1,27 @@
+<?php
+    define('PAGE_NAME', 'Bank | Home');
+    define('ABS_PATH', dirname(__FILE__));
+    extract($_REQUEST); // This is for grabbing the $_POST, $_GET or $_COOKIE
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bank | Home</title>
+    <title><?php echo PAGE_NAME ?></title>
 </head>
 <body>
     This is the home page.
+    <?php
+    if (isset($userName)){
+        echo "Welcome! $userName" . PHP_EOL;
+    } 
+    if (isset($userPassword)){
+        echo "Password was: $userPassword" . PHP_EOL;
+    }
 
+    ?>
     <a href="index.php">Click to go back</a>
 </body>
 </html>
--- banking/index.php
+++ banking/index.php
@@ -1,15 +1,21 @@
+<?php
+    define('PAGE_NAME', 'Bank | Login');
+    define('ABS_PATH', "../");
+    extract($_REQUEST); // This is for grabbing the $_POST, $_GET or $_COOKIE
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bank Login</title>
+    <title><?php echo PAGE_NAME ?></title>
 </head>
 <body>
-    <form  class="loginForm" action="bank.php" method="GET">
-        <label for="userName" >Username: <label><input type="text" id="userName" name="userName"><br>
-        <label for="userPassword" >Password: <label><input type="text" id="userPassword" name="userPassword"><br>
-        <input class="loginSubmit"type="submit" placeholder="Submit"> 
+    <form  class="loginForm" action=<?php echo ABS_PATH . "/bank.php"?> method="post">
+        <label for="userName" >Username: <input type="text" id="userName" name="userName"></label><br>
+        <label for="userPassword" >Password: <input type="password" id="userPassword" name="userPassword"></label><br>
+        <label for="userSubmit" ><input class="loginSubmit"type="submit" value="Submit"></label><br>
     </form>
     <a href="/CS3270/index.php"> Click to go back</a>
 </body>
